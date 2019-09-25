2c74f5d3a9b27a334622997c78f62cef3b8c80bf
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 15:17:09 -0400

    changed Nav and Jumbotron

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Clicky</h1>
+      <p>
+        This is an interactive game where you click for your favorite planet to
+        learn more!
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;
