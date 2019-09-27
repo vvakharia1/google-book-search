338c0de9b0e2085e4fd3a3b324064e36cdaef92c
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-27 10:14:28 -0400

    included styling within Nav

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,14 +1,35 @@
 import React from "react";
-import "./style.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import image from "../../assets/images/pages.jpg";
 
-function Jumbotron() {
+const buttons = {
+  margin: "1rem"
+};
+
+const backgroundImage = {
+  backgroundImage: `url(${image})`,
+  height: "300",
+  clear: "both",
+  paddingTop: "120",
+  textAlign: "center",
+  borderRadius: 0
+};
+
+function Jumbotron({ children }) {
   return (
-    <div className="jumbotron text-center">
-      <h1>Clicky</h1>
-      <p>
-        This is an interactive game where you click for your favorite planet to
-        learn more!
-      </p>
+    <div style={backgroundImage} className="jumbotron">
+      {children}
+
+      <Link to="/search">
+        <button style={buttons} type="button" className="btn btn-secondary">
+          Search for Books
+        </button>
+      </Link>
+      <Link to="/saved">
+        <button style={buttons} type="button" className="btn btn-secondary">
+          Saved Books
+        </button>
+      </Link>
     </div>
   );
 }
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+const navstyle = {
+  backgroundColor: "#D9002A"
+};
+
 function Nav() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">
-          React Reading List
-        </a>
-      </nav>
-    );
-  }
+  return (
+    <nav style={navstyle} className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand" href="/">
+        (React) Google Book Search
+      </a>
+    </nav>
+  );
+}
 
 export default Nav;
