cd35beda565f7c2f31f7332a7a74228414be1ff7
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-27 10:53:20 -0400

    changed dependecies in package json

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
